1ee92b9c3fb7192d3e2a74c2aab693ee67bfd838
kittyxcode <katherine.pino059@gmail.com>
2024-12-16 16:50:16 -0300

    feat: create CORS ERROR

--- FrontEnd/client/src/App.jsx
+++ FrontEnd/client/src/App.jsx
@@ -1,14 +1,11 @@
-//prueba
-
 function App(){
   return(
     <div>
       <h1>Click para enviar peticion</h1>
-      <button onClick={
-        async()=>{ 
-          const res = await fetch('http://localhost:3000/ping')}
-          }>Fetch</button>
-          
+      <button onClick={async()=>{
+        const res = await fetch('http://localhost:3000/ping')
+        console.log(res)
+      }}>Fecth</button>  
     </div>
   )
 }
