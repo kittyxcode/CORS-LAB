f175c1227e03ac49d41b3590600d1c1279963dfa
kittyxcode <katherine.pino059@gmail.com>
2024-12-16 17:27:57 -0300

    feat: better solution with library cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+/*app.use((req, res, next)=>{
+    res.append ('Access-Control-Allow-Origin', '*')
+    res.append('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+    //res.append ('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+})*/
+
+app.use(cors());
+
+app.get('/ping', (req, res) => {
+  res.send('Pong');
+});
+
 app.listen(3000);
-
-console.log('server on port 3000')
-
-app.get('/ping', (req, res) =>{
-    res.sendStatus('Pong')
-})
+console.log('server on port 3000');
